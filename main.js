ad52c428f98bfa908054907707b9df63eada9cf1
mihail2308 <155915865+mihail2308@users.noreply.github.com>
2024-01-10 16:55:35 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,71 +1,62 @@
-const apiCorseValyte =" https://www.cbr-xml-daily.ru/daily_json.js"
+let countDown ;
+const timerDisplay = document.querySelector(".timer_display-left")
+const endTime = document.querySelector(".timer_display-end")
+const timerForm = document.querySelector("#form")
 
-const elemUSD = document.querySelector(`[data-value="USD"]`)
-const elemEUR = document.querySelector(`[data-value="EUR"]`)
-const elemGBP = document.querySelector(`[data-value="GBP"]`)
-const elemBYN = document.querySelector(`[data-value="BYN"]`)
+ const buttons = document.querySelectorAll("[data-time]")
 
-const inputByn = document.querySelector("#input")
-const res = document.querySelector("#result")
-const select = document.querySelector("#select")
+console.log(timerForm);
+function timer (seconds){
+    clearInterval(countDown)
 
-const selectBYN = document.querySelector("#exampleFormControlSelect1")
+    const currentTime = Date.now()
+    const endTime = currentTime + seconds * 1000
+    displayTimer(seconds);
 
-
-
-const valut = {}
-async function converter () {
-
-const respons = await fetch (apiCorseValyte)
-const data =  await respons.json()
-const result = await data
-
-valut.USD = result.Valute.USD;
-valut.EUR = result.Valute.EUR;
-valut.BYN = result.Valute.BYN;
-valut.GBP = result.Valute.GBP;
-
-elemUSD.textContent = valut.USD.Value.toFixed(2);
-elemEUR.textContent = valut.EUR.Value.toFixed(2);
-elemGBP.textContent = valut.GBP.Value.toFixed(2);
-elemBYN.textContent = valut.BYN.Value.toFixed(2);
-
-addStyleValute()
-}
-converter ()
-
-function addStyleValute(){
-    if(valut.USD.Value > valut.USD.Previos){
-        elemUSD.classList.add("top")  
-    }else{
-        elemUSD.classList.add("bottom")  
-    }
-
-    if(valut.EUR.Value > valut.EUR.Previos){
-        elemEUR.classList.add("top")  
-    }else{
-        elemEUR.classList.add("bottom")  
-    }
-
-    if(valut.GBP.Value > valut.GBP.Previos){
-        elemGBP.classList.add("top")  
-    }else{
-        elemGBP.classList.add("bottom")  
-    }
-    if(valut.BYN.Value > valut.BYN.Previos){
-        elemBYN.classList.add("top")  
-    }else{
-        elemBYN.classList.add("bottom")  
-    }
-}
-    inputByn.oninput = function(){
-        if (selectBYN.value == "BYN") {
-            res.value = (parseFloat(inputByn.value) / valut[select.value].Value * 28.7 ).toFixed(2) 
-
-        }else if(selectBYN.value == "RUB"){
-
-           res.value = (parseFloat(inputByn.value) / valut[select.value].Value ).toFixed(2)
+    countDown = setInterval(() => {
+    const secondLeft = Math.round((endTime - Date.now()) / 1000);
+ 
+        if (secondLeft <= 0) {
+        clearInterval(countDown)
+        return;
         }
-        
+     displayTimer (secondLeft)
+     displayEndTime(endTime)
+
+    },1000)
+}
+// timer()
+// 
+function displayTimer (seconds){
+    const minutes = Math.floor(seconds / 60)
+    const lostSeconds = seconds % 60
+
+    const display = `${minutes}:${lostSeconds < 10 ? "0": ""}${lostSeconds}`
+    timerDisplay.textContent = display
+    console.log({minutes,lostSeconds});
+}
+
+function displayEndTime (timestamp){
+const end = new Date(timestamp)
+const hours = end.getHours()
+const minutes = end.getMinutes()
+
+endTime.textContent = `вернутся в ${hours}:${minutes < 10 ? "0":""}${minutes}`
+}
+
+function startTime() {
+    console.log(this);
+    const seconds = parseInt(this.dataset.time)
+    timer(seconds);
+
     }
-    
+buttons.forEach(button => button.addEventListener("click",startTime))
+
+timerForm.addEventListener('submit',function(e){
+    e.preventDefault()
+    const minutes =parseInt(this.minutes.value)
+    timer(minutes * 60)
+    this.reset()
+    console.log(this.minutes.value);
+  })
+
